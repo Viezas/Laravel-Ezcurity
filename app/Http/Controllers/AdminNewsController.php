54751422161dfe00893ee92ce2cc3af6ef92e223
Victor Ngauv <dxviezas@gmail.com>
2021-06-17 01:19:47 +0200

    Update AdminNewsController.php

--- app/Http/Controllers/AdminNewsController.php
+++ app/Http/Controllers/AdminNewsController.php
@@ -31,7 +31,6 @@ class AdminNewsController extends Controller
     {
         $article = News::where('id', $id)->get();
         $activatedNews = News::where('published', true)->get();
-        dd(count($activatedNews));
         if (!$request->img) {
             News::where('id', $id)->update([
                 'title' => $request->title,
